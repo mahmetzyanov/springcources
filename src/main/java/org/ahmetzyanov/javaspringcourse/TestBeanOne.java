3a4b5f891510135f1207c23cfefabe8525d0a084
Marat Ahmetzyanov <marat.ahmetzyanov@linius.com>
2019-09-04 16:36:42 +0500

    initialize springcources

---
+++ src/main/java/org/ahmetzyanov/javaspringcourse/TestBeanOne.java
@@ -0,0 +1,17 @@
+package org.ahmetzyanov.javaspringcourse;
+
+public class TestBeanOne {
+    private String name;
+
+    public TestBeanOne(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
